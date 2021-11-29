1dca1db67051f977630c48a0067e86a8637e47a1
lujc <lujc@inno-view.cn>
2021-11-29 16:11:32 +0800

    rename directory

--- git-address-rename/git_address_rename.py
+++ git-address-rename/git_address_rename.py
@@ -6,19 +6,19 @@ import os
 def replace_all_config_git_address(root_path, old_str, new_str):
     project_dirs = os.listdir(root_path)
     for project_dir in project_dirs:
-        config_path = root_path + "\\" + project_dir + "\\.git\\config"
+        config_path = root_path + '\\' + project_dir + '\\.git\\config'
         if os.path.exists(config_path):
             string_replace_in_file(config_path, old_str, new_str)
 
 
 def string_replace_in_file(file, old_str, new_str):
-    with open(file, "r", encoding="utf-8") as f1, open("%s.bak" % file, "w", encoding="utf-8") as f2:
+    with open(file, 'r', encoding='utf-8') as f1, open('%s.bak' % file, 'w', encoding='utf-8') as f2:
         for line in f1:
             if old_str in line:
                 line = line.replace(old_str, new_str)
             f2.write(line)
     os.remove(file)
-    os.rename("%s.bak" % file, file)
+    os.rename('%s.bak' % file, file)
 
 
 def read_config():
@@ -33,8 +33,8 @@ def read_config():
 if __name__ == '__main__':
     # 0. read config from json
     config = read_config()
-    path = config.get("root_path", os.getcwd())
-    original_address = config.get("original")
-    target_address = config.get("target")
+    path = config.get('root_path', os.getcwd())
+    original_address = config.get('original')
+    target_address = config.get('target')
 
     replace_all_config_git_address(path, original_address, target_address)
