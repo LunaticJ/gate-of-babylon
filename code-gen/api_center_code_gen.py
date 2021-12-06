f4a81003215ef76eec0b42428a3f2a24f59cd0ab
lujc <lujc@inno-view.cn>
2021-12-06 15:15:16 +0800

    add test rest and visualization api

--- code-gen/api_center_code_gen.py
+++ code-gen/api_center_code_gen.py
@@ -22,6 +22,8 @@ service_query_param = 'String {name}'
 
 controller_methods = []
 service_methods = []
+rest_client_methods = []
+api_infos = []
 
 
 def get_api_id_list(host, cookie, cat_id):
@@ -57,6 +59,7 @@ def read_config():
 
 def method_convert(api_info, config):
     root_path = config.get('root_path')
+    rest_client_method_template = open('template/rest_client_method.txt').read()
     controller_method_template = open('template/controller_method.txt').read()
     service_method_template = open('template/service_method.txt').read()
     input_check_template = open('template/input_check.txt').read()
@@ -74,15 +77,19 @@ def method_convert(api_info, config):
     method_input_desc_arr = []
     service_input_arr = []
     param_check_arr = []
+    rest_client_query_params = []
     for item in req_query:
         param_name = item.get('name')
         param_desc = item.get('desc')
         param_required = item.get('required')
+        param_example = item.get('example', None)
         method_input_desc_arr.append(input_param_desc.format(name=param_name, desc=param_desc))
         controller_query_params.append(controller_query_param.format(name=param_name))
         service_query_params.append(service_query_param.format(name=param_name))
         service_input_arr.append(param_name)
         if param_required == '1':
+            if param_example is not None:
+                rest_client_query_params.append(param_name + '=' + param_example)
             param_check_arr.append(input_check_template.format(param=param_name))
     controller_method = controller_method_template.format(method_type=method_type.upper(), path=path,
                                                           method_name=method_name,
@@ -95,8 +102,24 @@ def method_convert(api_info, config):
                                                     method_desc=method_desc,
                                                     input_desc=''.join(method_input_desc_arr),
                                                     service_input=', '.join(service_query_params))
+    rest_client_method_url = rest_client_method_template.format(method_name=method_name, method_desc=method_desc,
+                                                            method_type=method_type.upper(),
+                                                            test_host=config.get('test_host'), root_path=root_path,
+                                                            path=path)
+    api_info_url = 'http://' + config.get('api_host') + root_path + path
+    rest_client_method_query_params = '&'.join(rest_client_query_params)
+    if len(rest_client_method_query_params) is 0:
+        rest_client_method = rest_client_method_url
+    else:
+        rest_client_method = '?'.join([rest_client_method_url, rest_client_method_query_params])
+        api_info_url = '?'.join([api_info_url, rest_client_method_query_params])
     controller_methods.append(controller_method)
     service_methods.append(service_method)
+    rest_client_methods.append(rest_client_method)
+    api_infos.append({
+        'api_name': method_desc,
+        'api_url': api_info_url,
+    })
 
 
 if __name__ == '__main__':
@@ -106,7 +129,7 @@ if __name__ == '__main__':
 
     config = read_config()
     host = config.get("api_center_host")
-    cookie = config.get("cookie")
+    cookie = config.get("api_center_cookie")
     cat_id = config.get("cat_id")
     api_id_arr = get_api_id_list(host, cookie, cat_id)
     if len(api_id_arr) == 0:
@@ -129,9 +152,16 @@ if __name__ == '__main__':
         service_str = service_temp.format(package=package, class_name=class_name, class_desc=class_desc, date=date,
                                           method_arr='\n'.join(service_methods))
 
-        with open('output\\'+class_name+'Controller.java', 'w', encoding='utf-8') as cf:
+        rest_client_str = '\n'.join(rest_client_methods)
+        with open('output\\' + class_name + 'Controller.java', 'w', encoding='utf-8') as cf:
             cf.write(controller_str)
             cf.close()
         with open('output\\' + class_name + 'Service.java', 'w', encoding='utf-8') as sf:
-            sf.write(service_str);
+            sf.write(service_str)
             sf.close()
+        with open('output\\' + class_name + '.rest', 'w', encoding='utf-8') as rf:
+            rf.write(rest_client_str)
+            rf.close()
+        with open('output\\visualization_apis.json', 'w', encoding='utf-8') as af:
+            af.write(json.dumps(api_infos, ensure_ascii=False, indent=2))
+            af.close()
